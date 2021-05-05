65ea27723a99de9ae72dc86efd74ac769d8442fa
Elise Muellerleile <eliamue@live.com>
2021-05-05 13:46:36 -0500

    countdown now works

--- app.js
+++ app.js
@@ -7,26 +7,37 @@ const result = document.getElementById('attempts');
 const wrongGuess = document.getElementById('attempts');
 const resetButton = document.getElementById('reset-button');
 const winImg = document.getElementById('winner');
+const loseImg = document.getElementById('wangernumb');
 // initialize state
 
 let numGuess = 0;
 let correctNumber = Math.ceil(Math.random() * 20);
+let countGuess = 5;
 
 
 // set event listeners to update state and DOM
 button.addEventListener('click', () => {
     const guess = Number(userInput.value);
     numGuess = numGuess + 1;
+    countGuess = countGuess - 1;
 
-    if (compareNumbers(guess, correctNumber) === 0) {
+    if (countGuess === 0) {
+        wrongGuess.textContent = 'Oh no, You failed! That is Wangernumb, goodbye forever.';
+        numGuess = 0;
+        correctNumber = Math.ceil(Math.random() * 20);
+        winImg.style.display = 'none';
+        loseImg.style.display = 'block';
+        resetButton.style.display = 'block';
+
+    } else if (compareNumbers(guess, correctNumber) === 0) {
         result.textContent = 'You got Numberwang in ' + numGuess + ' tries!';
         winImg.style.display = 'block';
         resetButton.style.display = 'block';
     } else if (compareNumbers(guess, correctNumber) === 1) {
-        wrongGuess.textContent = guess + ' is too damn high. No Numberwang! You have tried ' + numGuess + ' times.';
+        wrongGuess.textContent = guess + ' is too damn high. No Numberwang! You have tried ' + numGuess + ' times and you have ' + countGuess + ' tries left';
         wrongGuess.style.display = 'block';
     } else if (compareNumbers(guess, correctNumber) === -1) {
-        wrongGuess.textContent = guess + ' is too low- no Numberwang for you! You have made ' + numGuess + ' attempts';
+        wrongGuess.textContent = guess + ' is too low- no Numberwang for you! You have made ' + numGuess + ' attempts and you have ' + countGuess + ' tries left';
         wrongGuess.style.display = 'block';
     }
     console.log(compareNumbers(guess, correctNumber));
@@ -37,4 +48,5 @@ resetButton.addEventListener('click', () => {
     wrongGuess.style.display = 'none';
     correctNumber = Math.ceil(Math.random() * 20);
     winImg.style.display = 'none';
+    loseImg.style.display = 'none';
 });
